cce48769a8c56f92b72b18e6fb27fd8a0fa4fb76
sixthkid <motherbennett@gmail.com>
2016-06-28 13:24:48 -0400

    added documentation to MoveMotors

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MoveMotors.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MoveMotors.java
@@ -1,129 +1,132 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * A object that moves the motors for a given period of time.
+ *
+ */
 public class MoveMotors {
-    protected DcMotor leftMotor;
-    protected DcMotor rightMotor;
-    protected Continue StopFor;
-    protected Variable Veribul;
-    protected void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences) {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private Continue StopFor;
+
+    /**
+     * Constructor that takes the references of the motors and a reference to the continue class.
+     * @param LeftDriveMotorReferences left motor reference
+     * @param RightDriveMotorReferences right motor reference
+     * @param StopForReferences continue object reference
+     */
+    public void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences) {
         leftMotor = LeftDriveMotorReferences;
         rightMotor = RightDriveMotorReferences;
         StopFor = StopForReferences;
     }
-    protected void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences, MotorDirection.RightMotorDirection rightMotorDirection) {
+
+    /**
+     * Constructor that takes the references of the motors and a reference to the continue class.
+     * It also takes in two booleans which set the direction the motors.
+     * @param LeftDriveMotorReferences left motor reference
+     * @param RightDriveMotorReferences right motor reference
+     * @param StopForReferences continue object reference
+     * @param leftMotorRev boolean, when true it sets the left motor to reverse
+     * @param rightMotorRev boolean, when true it sets the right motor to reverse
+     */
+    public void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences, boolean leftMotorRev, boolean rightMotorRev) {
         leftMotor = LeftDriveMotorReferences;
         rightMotor = RightDriveMotorReferences;
-        if(rightMotorDirection == MotorDirection.RightMotorDirection.FORWARD) {
-            leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        if(leftMotorRev) {
+            leftMotor.setDirection(DcMotor.Direction.REVERSE);
         }
         else {
+            leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        }
+        if(rightMotorRev) {
             leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        }
+        else {
+            leftMotor.setDirection(DcMotor.Direction.FORWARD);
         }
         StopFor = StopForReferences;
     }
-    protected void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences, MotorDirection.LeftMotorDirection leftMotorDirection) {
-        leftMotor = LeftDriveMotorReferences;
-        rightMotor = RightDriveMotorReferences;
-        if(leftMotorDirection == MotorDirection.LeftMotorDirection.FORWARD) {
-            leftMotor.setDirection(DcMotor.Direction.FORWARD);
-        }
-        else {
-            leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        }
-        StopFor = StopForReferences;
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(int power, int timeDriven, Direction dir) {
+        Drive((double) power, (double) timeDriven, dir);
     }
-    protected void MoveMotor(DcMotor LeftDriveMotorReferences, DcMotor RightDriveMotorReferences, Continue StopForReferences, MotorDirection.LeftMotorDirection leftMotorDirection, MotorDirection.RightMotorDirection rightMotorDirection) {
-        leftMotor = LeftDriveMotorReferences;
-        rightMotor = RightDriveMotorReferences;
-        if(leftMotorDirection == MotorDirection.LeftMotorDirection.FORWARD) {
-            leftMotor.setDirection(DcMotor.Direction.FORWARD);
-        }
-        else {
-            leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        }
-        if(rightMotorDirection == MotorDirection.RightMotorDirection.FORWARD) {
-            leftMotor.setDirection(DcMotor.Direction.FORWARD);
-        }
-        else {
-            leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        }
-        StopFor = StopForReferences;
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(double power, int timeDriven, Direction dir) {
+        Drive(power, (double) timeDriven,  dir);
     }
-
-    /****************************************************************************************/
-    /*Forward Methods*/
-    //Methods for moveing the robot forward
-    //Takes ints double and floats
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    protected void Drive(int power, int timeDriven, Direction dir) {
-        Veribul = Variable.INTEGER;
-        Drive((double) power, (double) timeDriven, Veribul, dir);
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(int power, double timeDriven, Direction dir) {
+        Drive((double) power, timeDriven,  dir);
     }
-    protected void Drive(double power, int timeDriven, Direction dir) {
-        Veribul = Variable.DOUBLE;
-        Drive(power, (double) timeDriven, Veribul, dir);
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(float power, int timeDriven, Direction dir) {
+        Drive((double) power, (double) timeDriven,  dir);
     }
-
-    protected void Drive(int power, double timeDriven, Direction dir) {
-        Veribul = Variable.INTEGER;
-        Drive((double) power, timeDriven, Veribul, dir);
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(float power, double timeDriven, Direction dir) {
+        Drive((double) power, timeDriven,  dir);
     }
-
-    protected void Drive(float power, int timeDriven, Direction dir) {
-        Veribul = Variable.FLOAT;
-        Drive((double) power, (double) timeDriven, Veribul, dir);
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(int power, float timeDriven, Direction dir) {
+        Drive((double) power, (double) timeDriven,  dir);
     }
-
-    protected void Drive(float power, double timeDriven, Direction dir) {
-        Veribul = Variable.FLOAT;
-        Drive((double) power, timeDriven, Veribul, dir);
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(double power, float timeDriven, Direction dir) {
+        Drive(power, (double) timeDriven, dir);
     }
-
-    protected void Drive(int power, float timeDriven, Direction dir) {
-        Veribul = Variable.INTEGER;
-        Drive((double) power, (double) timeDriven, Veribul, dir);
-    }
-
-    protected void Drive(double power, float timeDriven, Direction dir) {
-        Veribul = Variable.DOUBLE;
-        Drive(power, (double) timeDriven, Veribul, dir);
-    }
-
-    protected void Drive(double power, double timeDriven, Direction dir) {
-        Veribul = Variable.DOUBLE;
-        Drive(power, timeDriven, Veribul, dir);
-    }
-
-    //****************************************************************//
-    //this section runs the motors for a given time, and will move in the direction given.
-    //dir: A enum that determines the direction the robot will move.
-    //V: A enum that makes sure your varaibls are between two set numbers.
-    //
-    //
-    //
-    private void Drive(double power, double timeDriven, Variable V, Direction dir) {
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        switch (V) {
-            case DOUBLE: {
-                if (power > 1 || power < 0) {
-                    throw new IndexOutOfBoundsException("power must be between 1 and 0, while using doubles");
-                }
-            }
-            case FLOAT: {
-                if (power > 100 || power < 0) {
-                    throw new IndexOutOfBoundsException("power must be between 100 and 0, while using floats");
-                }
-                power = power / 100;
-            }
-            case INTEGER: {
-                if (power > 100 || power < 0) {
-                    throw new IndexOutOfBoundsException("power must be between 100 and 0, while using Integers");
-                }
-                power = power / 100;
-            }
-        }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     *  This method runs the motors in a given direction for a given period of time then stops.
+     *  Take note, turn left and turn right turn in place rather than one motor moving.
+     * @param power sets the power of the motors, ranging from 100 to 0.
+     * @param timeDriven sets the time the motors will run for, can not be less then 0.
+     * @param dir sets the direction the motors will run, FORWARD, LEFT, RIGHT and BACKWARD.
+     */
+    public void Drive(double power, double timeDriven, Direction dir) {
+        power = power / 100;
         switch (dir) {
             case FORWARD: {
                 leftMotor.setPower(power);
@@ -155,13 +158,13 @@ public class MoveMotors {
             }
         }
     }
+
+    /**
+     * the direction the robot will turn.
+     */
     public enum Direction {
         FORWARD, RIGHT, LEFT, BACKWARD
         //enum for MoveMotors, used to assign a direction.
-    }
-    public enum Variable {
-        DOUBLE, INTEGER, FLOAT
-        //enum for MoveMotors, used to change varibul numbers.
     }
 }
 
